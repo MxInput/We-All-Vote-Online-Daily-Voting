1e492058103e05aef996bed3c425af857028dad5
MxInput <mxinput@protonmail.com>
2026-05-19 21:20:18 -0400

    first commit
    I made the sign-in page and improved its aesthetics.
    I also started working on some initial questions for the voting mechanic.

---
+++ script.js
@@ -0,0 +1,64 @@
+function start() {
+    let loginForm = document.getElementById('loginForm')
+    let signUpForm = document.getElementById('signUpForm')
+
+    loginForm.addEventListener('submit', function (event) {
+        var username = document.getElementById('logUN').value;
+        var password = document.getElementById('logPW').value;
+    });
+
+    signUpForm.addEventListener('submit', function (event) {
+        var newUsername = document.getElementById('signUN').value;
+        var newPassword = document.getElementById('signPW').value;
+        var confirmPassword = document.getElementById('pWRepeat').value;
+
+        if (newPassword !== confirmPassword) {
+            alert('Passwords do not match.');
+            event.preventDefault();
+        }
+    });
+}
+
+function showSignUp() {
+    let signUpButton = document.getElementById('signUpBtn')
+    signUpButton.style.display = 'none'
+
+    let loginButton = document.getElementById('loginBtn')
+    loginButton.style.display = 'none'
+
+    let signUpBlock = document.getElementById('signUpBlock')
+    signUpBlock.style.display = 'block'
+}
+
+function closeSignUp() {
+    let signUpButton = document.getElementById('signUpBtn')
+    signUpButton.style.display = 'inline-block'
+
+    let loginButton = document.getElementById('loginBtn')
+    loginButton.style.display = 'inline-block'
+
+    let signUpBlock = document.getElementById('signUpBlock')
+    signUpBlock.style.display = 'none'
+}
+
+function showLogin() {
+    let signUpButton = document.getElementById('signUpBtn')
+    signUpButton.style.display = 'none'
+
+    let loginButton = document.getElementById('loginBtn')
+    loginButton.style.display = 'none'
+
+    let loginBlock = document.getElementById('loginBlock')
+    loginBlock.style.display = 'block'
+}
+
+function closeLogin() {
+    let signUpButton = document.getElementById('signUpBtn')
+    signUpButton.style.display = 'inline-block'
+
+    let loginButton = document.getElementById('loginBtn')
+    loginButton.style.display = 'inline-block'
+
+    let loginBlock = document.getElementById('loginBlock')
+    loginBlock.style.display = 'none'
+}
